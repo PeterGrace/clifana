a7d843341d51b7f1d5f0df041bc2085e985579b6
Peter Grace <pete.grace@gmail.com>
2023-04-08 01:20:46 +0000

    logging into log box works, ish

--- src/app_data.rs
+++ src/app_data.rs
@@ -1,11 +1,15 @@
+use std::borrow::Cow;
 use std::collections::VecDeque;
+use std::sync::{Arc, Mutex};
 use crate::consts::*;
-#[derive(Default)]
+use tui_menu::{MenuItem, MenuState};
+
 pub(crate) struct AppData {
     pub data: Vec<(f64, f64)>,
     pub screen_refresh_secs: u32,
     pub tick_interval_msecs: u64,
-    pub log_buffer: VecDeque<String>
+    pub log_buffer: Arc<Mutex<VecDeque<String>>>,
+    pub menu: MenuState<Cow<'static, str>>
 }
 impl AppData {
     pub(crate) fn default() -> AppData {
@@ -13,11 +17,24 @@ impl AppData {
             data: vec![],
             screen_refresh_secs: DEFAULT_SCREEN_REFRESH_RATE_SECS,
             tick_interval_msecs: DEFAULT_TICK_INTERVAL_MSECS,
-            log_buffer: VecDeque::with_capacity(DEFAULT_LOG_RING_BUFFER_SIZE)
+            log_buffer: Arc::new(Mutex::new(VecDeque::with_capacity(DEFAULT_LOG_RING_BUFFER_SIZE))),
+            menu: make_menu()
         }
     }
 
     pub(crate) fn on_tick(&mut self) {
         debug!("tick");
     }
+}
+fn make_menu()-> MenuState<Cow<'static, str>> {
+    MenuState::new(vec![
+        MenuItem::group(
+            "File",
+            vec![MenuItem::item("Exit", "exit".into())]
+        ),
+        MenuItem::group(
+            "Help",
+    vec![MenuItem::item("About", "about".into())]),
+
+    ])
 }
--- src/main.rs
+++ src/main.rs
@@ -28,10 +28,12 @@ use ratatui::{
     Frame, Terminal,
 };
 use std::{error::Error, io, panic, time::{Duration, Instant}};
+use std::borrow::Cow;
 use std::ops::Add;
 use ratatui::layout::Alignment;
-use ratatui::widgets::{Paragraph, Wrap};
+use ratatui::widgets::{LineGauge, Paragraph, Wrap};
 use app_data::AppData;
+use tui_menu::Menu;
 use crate::consts::DEFAULT_SCREEN_MARGIN;
 
 
@@ -40,7 +42,6 @@ extern crate log;
 
 
 fn main() -> anyhow::Result<()> {
-    // setup a hook when panic occurs
     better_panic::install();
     panic::set_hook(Box::new(|panic_info| {
         restore_terminal(Some(format!("Panic occurred")));
@@ -60,12 +61,17 @@ fn main() -> anyhow::Result<()> {
         _ => "DEBUG".to_string()
     };
     std::env::set_var("RUST_LOG", log_level);
+    let mut app = AppData::default();
+    let mut log_buffer = app.log_buffer.clone();
     fern::Dispatch::new()
-        .chain(fern::log_file("clifana.log").unwrap())
         .format(move |out, message, record| {
             out.finish(format_args!("[{}] {}", record.level(), message))
-        }).apply().unwrap();
-
+        })
+        .chain(fern::log_file("clifana.log").unwrap())
+        .chain(fern::Output::call(move |record| {
+            log_buffer.lock().unwrap().push_front(format!("{}", record.args()));
+        }))
+        .apply().unwrap();
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
@@ -76,7 +82,6 @@ fn main() -> anyhow::Result<()> {
             execute_query(&config, args)?;
         }
     };
-    let app = AppData::default();
     let tick_rate = Duration::from_millis(app.tick_interval_msecs);
     let res = run_app(&mut terminal, app, tick_rate);
     if res.is_err() {
@@ -111,21 +116,50 @@ fn run_app<B: Backend>(
 ) -> io::Result<()> {
     let mut last_tick = Instant::now();
     loop {
+
         terminal.draw(|f| ui(f, &mut app))?;
+
+        //region Keystroke Processing
         let timeout = tick_rate
             .checked_sub(last_tick.elapsed())
             .unwrap_or_else(|| Duration::from_secs(0));
         if crossterm::event::poll(timeout)? {
             if let Event::Key(key) = event::read()? {
-                if let KeyCode::Char('q') = key.code {
-                    return Ok(());
+                match key.code {
+                    KeyCode::Left => app.menu.left(),
+                    KeyCode::Right => app.menu.right(),
+                    KeyCode::Up => app.menu.up(),
+                    KeyCode::Down => app.menu.down(),
+                    KeyCode::Esc => app.menu.reset(),
+                    KeyCode::Enter => app.menu.select(),
+                    KeyCode::Char('q') => { return Ok(()) },
+                    _ => {}
                 }
             }
         };
+        //endregion
+
+        //region Menu Selection Drain
+        for e in app.menu.drain_events() {
+            match e {
+                tui_menu::MenuEvent::Selected(item) => match item.as_ref() {
+                    "exit" => {
+                        return Ok(());
+                    }
+                    _ => {
+                        // println!("{} selected", item);
+                    }
+                },
+            }
+        }
+        //endregion
+
+        //region application tick processing
         if last_tick.elapsed() >= tick_rate {
             app.on_tick();
             last_tick = Instant::now();
         }
+        //endregion
     }
 }
 
@@ -133,50 +167,94 @@ fn run_app<B: Backend>(
 // ui
 
 fn ui<B: Backend>(f: &mut Frame<B>, app: &mut AppData) {
+
+    //region Pane Setup
     let size = f.size();
     let panes = Layout::default()
         .direction(Direction::Vertical)
         .margin(DEFAULT_SCREEN_MARGIN)
         .constraints([
+            Constraint::Min(1),
             Constraint::Percentage(75),
-            Constraint::Percentage(25)
+            Constraint::Percentage(25),
+            Constraint::Min(1)
         ].as_ref()
         ).split(size);
+    //endregion
 
+    //region Line Chart
     let datasets = vec![
         Dataset::default()
             .name("results")
             .marker(symbols::Marker::Dot)
-            .style(Style::default().fg(Color::Cyan))
+            .style(Style::default()
+                .bg(Color::LightBlue)
+                .fg(Color::Cyan))
             .data(&app.data)];
-
     let chart = Chart::new(datasets)
         .block(create_block("Results"));
+    f.render_widget(chart, panes[1]);
+    //endregion
+
+    //region Log Pane
     let mut loop_exit: bool = false;
     let mut log_paragraph: String = "".to_string();
-    while (app.log_buffer.len() > 0) || (loop_exit) {
-        let next = match app.log_buffer.pop_back() {
-            Some(s) => s,
-            None => { break;}
-        };
-        log_paragraph = log_paragraph.add(&next);
-    };
+    let mut log_buffer = app.log_buffer.lock().unwrap();
 
+    log_paragraph = itertools::join(log_buffer.iter(), "\n");
+    // while (log_buffer.len() > 0) || (loop_exit) {
+    //
+    //     let next = match log_buffer.pop_back() {
+    //         Some(s) => s,
+    //         None => { break; }
+    //     };
+    //     log_paragraph = log_paragraph.add(&next);
+    // };
     let log = Paragraph::new(log_paragraph)
         .style(Style::default().bg(Color::LightBlue).fg(Color::Gray))
         .block(create_block("Execution Log"))
         .alignment(Alignment::Left)
         .wrap(Wrap { trim: true });
-    f.render_widget(chart, panes[0]);
-    f.render_widget(log, panes[1]);
+    f.render_widget(log, panes[2]);
+    //endregion
+
+    //region Bottom Status Line
+    let bottom_line = Paragraph::new("Bottom Line");
+    f.render_widget(bottom_line, panes[3]);
+    //endregion
+
+    //region Top Menubar
+    let menu = tui_menu::Menu::new()
+        .default_style(Style::default().bg(Color::White).fg(Color::Red));
+    f.render_stateful_widget(menu, panes[0], &mut app.menu);
+    //endregion
 }
 
-fn create_block(title: &str) -> Block{
-Block::default()
-.borders(Borders::ALL)
-.style(Style::default().bg(Color::White).fg(Color::Black))
-.title(Span::styled(
-title,
-Style::default().add_modifier(Modifier::BOLD),
-))
+fn create_block(title: &str) -> Block {
+    Block::default()
+        .borders(Borders::ALL)
+        .title_alignment(Alignment::Center)
+        .style(
+            Style::default()
+                .bg(Color::LightBlue)
+                .fg(Color::White))
+        .title(Span::styled(
+            title,
+            Style::default()
+                .add_modifier(Modifier::BOLD),
+        ))
+}
+fn create_dialog_block(title: &str) -> Block {
+    Block::default()
+        .borders(Borders::ALL)
+        .title_alignment(Alignment::Center)
+        .style(
+            Style::default()
+                .bg(Color::Gray)
+                .fg(Color::White))
+        .title(Span::styled(
+            title,
+            Style::default()
+                .add_modifier(Modifier::BOLD),
+        ))
 }
